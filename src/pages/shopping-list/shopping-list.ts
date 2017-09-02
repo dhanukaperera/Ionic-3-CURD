34f8326b39aa68d0afabad6af631ee737acece03
dhanukaperera <dhanukashehanperera@gmail.com>
2017-09-02 20:02:25 +0530

    Displaying Shopping List Data

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AddShoppingPage } from '../add-shopping/add-shopping';
+import { AngularFireDatabase ,FirebaseListObservable} from 'angularfire2/database/';
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 
 @Component({
   selector: 'page-shopping-list',
@@ -8,7 +10,12 @@ import { AddShoppingPage } from '../add-shopping/add-shopping';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  shoppingListRef$:FirebaseListObservable<ShoppingItem[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database:AngularFireDatabase) {
+    // Point shoppingListRef$ at Firebase -> 'shoping-list' node.  
+    this.shoppingListRef$ = this.database.list('shopping-list');
+    
   }
 
   navigateToAddShopingPage(){
