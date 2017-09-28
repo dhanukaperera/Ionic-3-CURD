3f6ebd3874367df4a07435338286d92e5733b0d6
dhanukaperera <dhanukashehanperera@gmail.com>
2017-09-28 23:56:49 +0530

    first commit

--- src/pages/edit-shopping-item/edit-shopping-item.ts
+++ src/pages/edit-shopping-item/edit-shopping-item.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database/';
 import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-edit-shopping-item',
@@ -9,18 +10,27 @@ import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface
 })
 export class EditShoppingItemPage {
 
+  shoppingItemSubscription:Subscription;
   shoppingItemRef$:FirebaseObjectObservable<ShoppingItem>;
-
-
+  shoppingItem = {} as ShoppingItem
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private database:AngularFireDatabase) {
     const shoppingItemId = this.navParams.get('shoppingItemId');
     console.log(shoppingItemId);
     this.shoppingItemRef$ = this.database.object(`shopping-list/${shoppingItemId}`);
+
+    this.shoppingItemSubscription =  this.shoppingItemRef$.subscribe(shoppingItem => this.shoppingItem =shoppingItem);
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditShoppingItemPage');
+
+  editShoppingItem(shoppingItem : ShoppingItem){
+    this.shoppingItemRef$.update(shoppingItem);
+    this.navCtrl.pop();
+  }
+
+  ionViewWillLeave(){
+    this.shoppingItemSubscription.unsubscribe();
   }
 
 }
